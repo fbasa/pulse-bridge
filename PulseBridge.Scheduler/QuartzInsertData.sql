16e7d81c721420a792a714a11541ab38b0ba23c8
Franklin Basa <basa.franklin@gmail.com>
2025-09-04 20:04:42 +0800

    added sql file

---
+++ PulseBridge.Scheduler/QuartzInsertData.sql
@@ -0,0 +1,27 @@
+USE [QuartzNet]
+GO
+
+INSERT INTO [dbo].[QRTZ_JobQueue]
+           ([JobType]
+           ,[Payload]
+           ,[Status]
+           ,[Attempts]
+           ,[AvailableAt]
+           ,[LockedAt]
+           ,[LockedBy]
+           ,[DispatchedAt]
+           ,[LastError])
+     VALUES
+           ('SignalR'
+           ,'Test payload'
+           ,0
+           ,0
+           ,GETUTCDATE()
+           ,null
+           ,null
+           ,null
+           ,null
+           )
+GO
+
+
