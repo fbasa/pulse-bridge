28e51a5a812c76fceefe49b5cd0504f3d508d49b
Franklin Basa <basa.franklin@gmail.com>
2025-09-10 18:19:13 +0800

    updated

--- PulseBridge.Web/src/environments/environment.ts
+++ PulseBridge.Web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   hubUrl_: 'https://localhost:7001/hubs/schedulerHub', // <-- change to your API's hub URL
-  hubUrl: 'http://api.localtest.me/hubs/schedulerHub'
+  hubUrl: 'http://localhost:8082/hubs/schedulerHub'
 };
