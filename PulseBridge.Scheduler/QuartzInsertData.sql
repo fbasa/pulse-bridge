509cb42952e51c4ecf5320eee9894d7e26f81ed5
Franklin Basa <basa.franklin@gmail.com>
2025-09-13 12:55:50 +0800

    update docker compose file

--- PulseBridge.Scheduler/QuartzInsertData.sql
+++ PulseBridge.Scheduler/QuartzInsertData.sql
@@ -1,25 +1,5 @@
 
-INSERT INTO [dbo].[QRTZ_JobQueue]
-           ([JobType]
-           ,[Payload]
-           ,[Status]
-           ,[Attempts]
-           ,[AvailableAt]
-           ,[LockedAt]
-           ,[LockedBy]
-           ,[DispatchedAt]
-           ,[LastError])
-     VALUES
-           ('SignalR'
-           ,'Test payload'
-           ,0
-           ,0
-           ,GETUTCDATE()
-           ,null
-           ,null
-           ,null
-           ,null
-           )
+INSERT INTO [dbo].[QRTZ_JobQueue]([JobType],[Payload],[Status],[Attempts],[AvailableAt])VALUES('SignalR','Test payload',0,0,GETUTCDATE())
 
 
 
