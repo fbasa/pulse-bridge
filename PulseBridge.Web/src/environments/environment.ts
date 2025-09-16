6224ed939391e3e2d57994a6007fe3732330321f
Franklin Basa <basa.franklin@gmail.com>
2025-09-16 14:44:12 +0800

    updated

--- PulseBridge.Web/src/environments/environment.ts
+++ PulseBridge.Web/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     hubUrl: 'https://api.localtest.me/hubs/schedulerHub',
     auth: {
-        issuer: 'https://idp.localtest.me', // OpenIddict Issuer (https://idp.localtest.me/.well-known/openid-configuration)
+        issuer: 'https://idp.localtest.me/', // OpenIddict Issuer (https://idp.localtest.me/.well-known/openid-configuration)
         clientId: 'angular-spa', // MUST exist in OpenIddict applications
         redirectUri: 'https://ui.localtest.me/auth/callback',//["http://localhost:4200/auth/callback"]
         postLogoutRedirectUri: 'https://ui.localtest.me/',
